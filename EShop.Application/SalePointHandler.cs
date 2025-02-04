97554622c83f9c4389918f3d2013fa4a438d31f8
leschinskiy.av <leschinskiy.av@dns-shop.ru>
2025-02-04 11:47:59 +1000

    сделал домашнее задание 3

--- EShop.Application/SalePointHandler.cs
+++ EShop.Application/SalePointHandler.cs
@@ -4,22 +4,40 @@ namespace EShop.Application
 {
     public class SalePointHandler
     {
-        private SalePoint[] products =
-        [
-            new SalePoint { Id = 1, Name = "Спортивная", SaleArea = 333 },
-            new SalePoint { Id = 2, Name = "Гоголя", SaleArea = 444 },
-            new SalePoint { Id = 3, Name = "Максим", SaleArea = 555 },
-            new SalePoint { Id = 4, Name = "Бачурин", SaleArea = 666 },
-            new SalePoint { Id = 5, Name = "Авангард", SaleArea = 777 }
-        ];
+        private readonly ISalePointRepository _salePointRepository;
+
+        public SalePointHandler(ISalePointRepository salePointRepository)
+        {
+            _salePointRepository = salePointRepository;
+        }
 
         public IEnumerable<SalePoint> Get()
         {
-            var mutatedArray = new List<SalePoint>();
-            foreach (SalePoint p in products)
-                mutatedArray.Add(new SalePoint { Id = p.Id, Name = p.Name, SaleArea = p.SaleArea * 100 });
+            var salePoints = _salePointRepository.GetAll();
+            return MutateSalePoints(salePoints);
+        }
 
-            return mutatedArray;
+        public IEnumerable<SalePoint> GetById(int? id)
+        {
+            var salePoints = _salePointRepository.GetById(id);
+            return MutateSalePoints(salePoints);
+        }
+
+        private IEnumerable<SalePoint> MutateSalePoints(IEnumerable<SalePoint> salePoints)
+        {
+            var mutatedList = new List<SalePoint>();
+
+            foreach (var point in salePoints)
+            {
+                mutatedList.Add(new SalePoint
+                {
+                    Id = point.Id,
+                    Name = point.Name,
+                    SaleArea = point.SaleArea * 100
+                });
+            }
+
+            return mutatedList;
         }
     }
 }
